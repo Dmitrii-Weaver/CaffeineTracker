0db63f7e02e9be22621b9b0b55946f4ea706d12b
Nabeel Hussain <nabeelbaig962@gmail.com>
2024-10-14 03:47:27 +0300

    updated auth validation.

--- caffeinetracker/hooks/useHandleAuth.ts
+++ caffeinetracker/hooks/useHandleAuth.ts
@@ -1,42 +1,33 @@
 import React from 'react'
 import { firestore } from "../firebaseConfig";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from '@firebase/auth';
+import { Auth, User, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from '@firebase/auth';
 import { collection, doc, getDocs, query, setDoc, where } from "firebase/firestore";
+import { router } from 'expo-router';
 
 
 
 const useHandleAuth = () => {
-    const handleAuth = async (user: any, auth: any, isLogin: any, email: string, password: string, username: string) => {
+    const handleAuth = async (user: User | null, auth: Auth, isLogin: boolean, email: string, password: string, username: string) => {
         try {
             if (user) {
-                // If user is already authenticated, log out
-                console.log('User logged out successfully!');
                 await signOut(auth);
+                router.replace('/signin');
             } else {
-                // Sign in or sign up
                 if (isLogin) {
-                    // Sign in
                     await signInWithEmailAndPassword(auth, email, password);
-                    console.log('User signed in successfully!');
+                    router.replace('/');
                 } else {
-                    // Sign up
-                    const newUser = await createUserWithEmailAndPassword(auth, email, password);
-
-                    const usersRef = collection(firestore, "users")
-                    const userDoc = {
-                        uid: newUser.user.uid,
-                        email: newUser.user.email,
-                        username: username,
-                        coffees: []
-                    }
-                    await setDoc(doc(firestore, "users", newUser.user.uid), userDoc)
-                    localStorage.setItem("user-info", JSON.stringify(userDoc))
-
-                    console.log('User created successfully!');
+                    await createUserWithEmailAndPassword(auth, email, password);
+                    router.replace('/');
                 }
             }
+            return { success: true };
         } catch (error: any) {
-            console.error('Authentication error:', error.message);
+            console.error('Authentication error:', error);
+            console.error('Error code:', error.code);
+            console.error('Error message:', error.message);
+
+            return { error: 'Invalid email or password. Please try again.' };
         }
     }
     return handleAuth
