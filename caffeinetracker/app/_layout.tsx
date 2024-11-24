0dc11e62fa34f7759417c0dad5cf95824ad2772b
Nabeel_Hussain <nabeelbaig962@gmail.com>
2024-11-25 01:29:01 +0200

    fixed platform error of google sign-in for web which was causing the app to not run in the native environment.

--- caffeinetracker/app/_layout.tsx
+++ caffeinetracker/app/_layout.tsx
@@ -12,13 +12,14 @@ import React from 'react';
 import { UserProvider } from '@/store';
 import { Home } from '@/components/Home';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Platform } from 'react-native';
 
 // Configure Google Sign-In outside of the component
 GoogleSignin.configure({
-  webClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID
+  webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
+  offlineAccess: true,
 });
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -26,8 +27,6 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
-
 
   useEffect(() => {
     if (loaded) {
@@ -39,25 +38,28 @@ export default function RootLayout() {
     return null;
   }
 
- console.log(process.env.EXPO_PUBLIC_WEB_CLIENT_ID)
+  if (!process.env.EXPO_PUBLIC_WEB_CLIENT_ID) {
+    console.log('Client ID is empty');
+    return null;
+  }
 
- if (!process.env.EXPO_PUBLIC_WEB_CLIENT_ID) {
-
-  console.log('Client ID is empty')
-
-  return null
- }
-  
- return (
-    <GoogleOAuthProvider clientId={process.env.EXPO_PUBLIC_WEB_CLIENT_ID}>
-      <UserProvider>
-        <GluestackUIProvider config={config}>
-          <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-            <Home />
-          </ThemeProvider>
-        </GluestackUIProvider>
-      </UserProvider>
-    </GoogleOAuthProvider>
-
+  const content = (
+    <UserProvider>
+      <GluestackUIProvider config={config}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Home />
+        </ThemeProvider>
+      </GluestackUIProvider>
+    </UserProvider>
   );
+
+  if (Platform.OS === 'web') {
+    return (
+      <GoogleOAuthProvider clientId={process.env.EXPO_PUBLIC_WEB_CLIENT_ID}>
+        {content}
+      </GoogleOAuthProvider>
+    );
+  }
+
+  return content;
 }
